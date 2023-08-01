5f96b21cc7a51e078d4d468f8077cf875d34ee2e
armdupre <a-joseph.dupre@keysight.com>
2023-08-01 15:13:43 -0400

    Updated.

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "AmiName" {
-	default = "cyperf-mdw-1-0-10121-releasecyperf21-92384358-5015-4a4b-aed2-8113e28d871a"
+	default = "cyperf-mdw-1-0-11322-releasecyperf25-92384358-5015-4a4b-aed2-8113e28d871a"
 	description = "AMI name used for deploying instances"
 	type = string
 }
@@ -27,12 +27,27 @@ variable "InstanceId" {
 }
 
 variable "InstanceType" {
-	default = "m5.xlarge"
+	default = "c5.2xlarge"
 	description = "Instance type of VM"
 	type = string
 	validation {
-		condition = can(regex("t3.xlarge", var.InstanceType)) || can(regex("m5.xlarge", var.InstanceType))
-		error_message = "AppInstanceType must be one of (t3.xlarge | m5.xlarge) types."
+		condition = contains([	"m3.2xlarge",
+								"m4.2xlarge", "m4.4xlarge",
+								"m5.2xlarge", "m5.4xlarge", "m5.12xlarge",
+								"c3.2xlarge", "c3.4xlarge", "c3.8xlarge",
+								"c4.2xlarge", "c4.4xlarge", "c4.8xlarge",
+								"c5.2xlarge", "c5.4xlarge", "c5.9xlarge", "c5.12xlarge",
+								"c5n.2xlarge", "c5n.4xlarge", "c5n.9xlarge"
+							], var.InstanceType)
+		error_message = <<EOF
+InstanceType must be one of the following types:
+	m4.2xlarge, m4.4xlarge,
+	m5.2xlarge, m5.4xlarge, m5.12xlarge,
+	c3.2xlarge, c3.4xlarge, c3.8xlarge,
+	c4.2xlarge, c4.4xlarge, c4.8xlarge,
+	c5.2xlarge, c5.4xlarge, c5.9xlarge, c5.12xlarge,
+	c5n.2xlarge, c5n.4xlarge, c5n.9xlarge
+		EOF
 	}
 }
 
