23e9c002a8e1697cea964c54f37d2bcfcad0798c
armdupre <a-joseph.dupre@keysight.com>
2023-09-29 13:10:36 -0400

    Updated.

--- output.tf
+++ output.tf
@@ -8,10 +8,11 @@ output "Ami" {
 }
 
 output "Eth0ElasticIp" {
+	count = local.Eth0EnableElasticIp == true ? 1 : 0
 	description = "Elastic Ip resource associated with the first network interface."
 	value = {
-		"public_dns" : "https://${aws_eip.Eth0ElasticIp[*].public_dns}"
-		"public_ip" : aws_eip.Eth0ElasticIp[*].public_ip
+		"public_dns" : "https://${aws_eip.Eth0ElasticIp[0].public_dns}"
+		"public_ip" : aws_eip.Eth0ElasticIp[0].public_ip
 	}
 }
 
