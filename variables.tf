9743139b2e59a37b5df81df80f854f30c3384af5
armdupre <a-joseph.dupre@keysight.com>
2023-09-29 14:17:37 -0400

    Updated.

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "AmiName" {
-	default = "cyperf-mdw-1-0-11322-releasecyperf25-92384358-5015-4a4b-aed2-8113e28d871a"
+	default = "cyperf-mdw-1-0-10121-releasecyperf21-92384358-5015-4a4b-aed2-8113e28d871a"
 	description = "AMI name used for deploying instances"
 	type = string
 }
