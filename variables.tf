92ad1ab727ec7f02a809969fd8c8cefde0ed6e46
armdupre <a-joseph.dupre@keysight.com>
2023-10-02 15:40:37 -0400

    Updated.

--- variables.tf
+++ variables.tf
@@ -32,6 +32,12 @@ variable "InstanceId" {
 	type = string
 }
 
+variable "InstanceName" {
+	default = null
+	description = "Custom instance name used to override the default automatically generated name"
+	type = string
+}
+
 variable "InstanceType" {
 	default = "c5.2xlarge"
 	description = "Instance type of VM"
